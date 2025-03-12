c94dc9b017f5b71cf92814601fb46c9a10844f0e
alserene <aserene@swin.edu.au>
2025-03-12 10:18:20 +1000

    Create a recursion only variant.

--- src/gammainc/custom_upper_inc_gamma.py
+++ src/gammainc/custom_upper_inc_gamma.py
@@ -11,37 +11,12 @@ jax.config.update("jax_enable_x64", True) # NOTE: If comment this out, also upda
 jax.config.update("jax_disable_jit", True)
 
 
-def s_zero(x: ArrayLike) -> Array:
-    """Solution when s = 0."""
-
-    # TODO: Move typing to custom_gammaincc if possible
-    # Ensure x is a JAX double
-    x = jnp.array(x, dtype=jnp.float64)
-
-    # TODO: handle JAX bug
-    return -expi(-x)
-
-def s_half(x):
-    """Solution when s = 1/2."""
-
-    # Can use any of the following:
-    # lax.erfc(x)
-    # jax.scipy.special.erfc(x)
-    # 1 - jax.scipy.special.erf(x)
-
-    return jnp.sqrt(jnp.pi) * lax.erfc(jnp.sqrt(x))
-
-def s_one(x):
-    """Solution when s = 1."""
-
-    return jnp.exp(-x)
-
 def s_positive(s: ArrayLike, x: ArrayLike) -> Array:
 
     # lax.gammaincc is regularised by 1/Γ(s), so we multiply by Γ(s)
     return gamma(s) * (gammaincc(s, x))
 
-def s_negative(s: ArrayLike, x: ArrayLike, depth) -> Array:
+def s_negative(s: ArrayLike, x: ArrayLike) -> Array:
 
     def recur(gamma, s, x):
         return lax.div(gamma - jnp.pow(x, s) * lax.exp(-x), s)
@@ -59,17 +34,19 @@ def s_negative(s: ArrayLike, x: ArrayLike, depth) -> Array:
         return (new_gamma, s - 1), new_gamma
     
     # Get recursion starting values
+    recur_depth = 4
     s_start = s % 1.0
-    gamma_start = custom_gammaincc(s_start, x, depth)
+    # gamma_start = custom_gammaincc(s_start, x, recur_depth)
+    gamma_start = s_positive(s_start, x)
     
     # Initiate recursion
     initial_carry = (gamma_start, s_start)
-    result, _ = lax.scan(compute_recurrence, initial_carry, None, length=depth)
+    result, _ = lax.scan(compute_recurrence, initial_carry, None, length=recur_depth)
 
     # Return final gamma value
     return result[0]
 
-def custom_gammaincc(s: ArrayLike, x: ArrayLike, depth) -> Array:
+def custom_gammaincc(s: ArrayLike, x: ArrayLike) -> Array:
     """
     Computes the upper incomplete gamma function Γ(s, x) for any real s and real x >= 0.
 
@@ -89,13 +66,6 @@ def custom_gammaincc(s: ArrayLike, x: ArrayLike, depth) -> Array:
     - [s > 0] - Uses recurrence relation for all negative s, including negative integers.
     - [s < 0] - Handles the special case of s = 0 correctly using the exponential integral.
 
-
-    - [s = 0] - Uses the regular gamma function for positive s and non-integer s < 0
-    - [s = 0.5] - Returns inf when s = 0 or integer s < 0.
-    - [s = 1] - Uses regularised lower incomplete gamma function for positive s.
-    - [s > 0] - Uses recurrence relation for all negative s, including negative integers.
-    - [s < 0] - Handles the special case of s = 0 correctly using the exponential integral.
-
     Parameters:
     s (float): The shape parameter.
     x (float): The lower limit of integration.
@@ -106,44 +76,46 @@ def custom_gammaincc(s: ArrayLike, x: ArrayLike, depth) -> Array:
     
     s, x = promote_args_inexact("custom_gammaincc", s, x)
 
-    return lax.cond(
-        jnp.isinf(x), # x = inf
-        lambda _: 0,
-        lambda _: lax.cond(
-            x == 0, # x = 0 cases
-            lambda _: lax.cond(
-                s == 0, # s = 0
-                lambda _: jnp.inf,
-                lambda _: lax.cond(
-                    jnp.logical_and(s < 0, s == jnp.floor(s)), # s (int) < 0
-                    lambda _: lax.complex(jnp.inf, jnp.inf),
-                    lambda _: gamma(s), # s > 0 & s (non-int) < 0
-                    operand=None
-                ),
-                operand=None
-            ),
-            lambda _: lax.cond( # x > 0 cases
-                s == 0, # s = 0
-                lambda _: s_zero(x),
-                lambda _: lax.cond(
-                    s == 1/2, # s = 1/2
-                    lambda _: s_half(x),
-                    lambda _: lax.cond(
-                        s == 1, # s = 1
-                        lambda _: s_one(x),
-                        lambda _: lax.cond(
-                            s > 0, # s > 0
-                            lambda _: s_positive(s, x),
-                            lambda _: s_negative(s, x, depth), # s < 0
-                            operand=None
-                        ),
-                        operand=None
-                    ),
-                    operand=None
-                ),
-                operand=None
-            ),
-            operand=None
-        ),
-        operand=None
-    )
+    return s_negative(s, x)
+
+    # return lax.cond(
+    #     jnp.isinf(x), # x = inf
+    #     lambda _: 0,
+    #     lambda _: lax.cond(
+    #         x == 0, # x = 0 cases
+    #         lambda _: lax.cond(
+    #             s == 0, # s = 0
+    #             lambda _: jnp.inf,
+    #             lambda _: lax.cond(
+    #                 jnp.logical_and(s < 0, s == jnp.floor(s)), # s (int) < 0
+    #                 lambda _: lax.complex(jnp.inf, jnp.inf),
+    #                 lambda _: gamma(s), # s > 0 & s (non-int) < 0
+    #                 operand=None
+    #             ),
+    #             operand=None
+    #         ),
+    #         lambda _: lax.cond( # x > 0 cases
+    #             s == 0, # s = 0
+    #             lambda _: s_zero(x),
+    #             lambda _: lax.cond(
+    #                 s == 1/2, # s = 1/2
+    #                 lambda _: s_half(x),
+    #                 lambda _: lax.cond(
+    #                     s == 1, # s = 1
+    #                     lambda _: s_one(x),
+    #                     lambda _: lax.cond(
+    #                         s > 0, # s > 0
+    #                         lambda _: s_positive(s, x),
+    #                         lambda _: s_negative(s, x), # s < 0
+    #                         operand=None
+    #                     ),
+    #                     operand=None
+    #                 ),
+    #                 operand=None
+    #             ),
+    #             operand=None
+    #         ),
+    #         operand=None
+    #     ),
+    #     operand=None
+    # )
--- tests/test_upper_inc_gamma.py
+++ tests/test_upper_inc_gamma.py
@@ -8,15 +8,12 @@ from sympy import uppergamma, zoo
 from gammainc import custom_gammaincc
 
 
-@pytest.mark.parametrize("x", [0, 0.1, 0.5, 0.9, 1, 2.1, 3.7, 4.5, 5, 6.6, 7.1, 8.00005, 9.9, 10.0, 15, 18, 20, 25, 30, 100])
-@pytest.mark.parametrize("s", [-100, -5, -3.4, -1.2, -1, 0, 0.5, 1, 30, 100, 1000])
+@pytest.mark.parametrize("x", [0.1, 0.5, 0.9, 1, 2.1, 3.7, 4.5, 5, 6.6, 7.1, 8.00005, 9.9, 10.0, 15, 18, 20, 25, 30, 100, 1000])
+@pytest.mark.parametrize("s", [-3.4, -1.2, -1, 0, 0.5, 1, 1.7, 3])
 def test_inputs(s, x):
 
-    # Get recursion depth from s
-    d = np.abs(np.floor(s)).astype(int)
-
     # Call custom function
-    result_jax = custom_gammaincc(s, x, d)
+    result_jax = custom_gammaincc(s, x)
     # Call SymPy function
     result_sympy = uppergamma(s, x).evalf()
 
@@ -40,39 +37,10 @@ def test_inputs(s, x):
     else: # Remaining cases
         assert float(result_jax) == pytest.approx(float(result_sympy))
 
-# The JAX expi function has a bug; this test checks if bug exists.
-@pytest.mark.parametrize("x", [0.5])
-def test_jax_expi_jit_disabled_bug(x):
-
-    x = jnp.array(x, dtype=jnp.float64)
-
-    jax.config.update("jax_disable_jit", True)
-    res_jit_disabled = -jax.scipy.special.expi(-x)
-    jax.config.update("jax_disable_jit", False)
-    res_jit_enabled = -jax.scipy.special.expi(-x)
-
-    # Excessive print statements to help find the results among the test outputs.
-    print("\n\n\n\n\n")
-    print("#####     #####     #####     #####     #####     #####     #####     #####")
-    print("\n\n")
-    print("     git disabled + jax.scipy.special.expi --> bug ")
-    print("     ---------------------------------------------")
-    print("\n")
-    print(f"     jit enabled:      {res_jit_enabled}")
-    print(f"     jit disabled:     {res_jit_disabled}")
-    print("\n")
-    print("      expected result:  0.55977359")
-    print("\n\n")   
-    print("#####     #####     #####     #####     #####     #####     #####     #####")
-    print("\n\n\n\n\n")
-
-    assert res_jit_enabled == pytest.approx(0.55977359)
-    assert res_jit_disabled == pytest.approx(0.55977359)
-
 def test_grad():
     """Test that the custom gamma function can be autodifferentiated."""
 
     jax.config.update("jax_disable_jit", True)
 
     grad_custom_gammaincc = jax.grad(custom_gammaincc)
-    print(grad_custom_gammaincc(-1.2, 2.0, 2))
+    print(grad_custom_gammaincc(-1.2, 2.0))
