70b128af8a4fa9253ddb4e86c762eadd6f6d70f9
alserene <aserene@swin.edu.au>
2025-03-21 08:14:59 +1000

    Correct docstring to match recur only case.

--- src/gammainc/custom_upper_inc_gamma.py
+++ src/gammainc/custom_upper_inc_gamma.py
@@ -7,8 +7,7 @@ from jax.scipy.special import gamma, gammaincc, expi
 from jax._src.typing import Array, ArrayLike
 from jax._src.numpy.util import promote_args_inexact
 
-jax.config.update("jax_enable_x64", True) # NOTE: If comment this out, also update conversion in s_zero(x)
-jax.config.update("jax_disable_jit", True)
+jax.config.update("jax_enable_x64", True)
 
 
 def s_positive(s: ArrayLike, x: ArrayLike) -> Array:
@@ -16,19 +15,21 @@ def s_positive(s: ArrayLike, x: ArrayLike) -> Array:
     # lax.gammaincc is regularised by 1/Γ(s), so we multiply by Γ(s)
     return gamma(s) * (gammaincc(s, x))
 
-def s_negative(s: ArrayLike, x: ArrayLike) -> Array:
+def compute_gamma(s: ArrayLike, x: ArrayLike) -> Array:
 
     def recur(gamma, s, x):
         return lax.div(gamma - jnp.pow(x, s) * lax.exp(-x), s)
-        
+
     def compute_recurrence(carry, _):
         gamma, s = carry
 
-        # Handle case of JAX inf
-        if jnp.isinf(gamma):
-            new_gamma = jnp.inf
-        else:
-            new_gamma = recur(gamma, s - 1, x)
+        # Handle case of JAX inf        
+        new_gamma = lax.cond(
+            jnp.isinf(gamma),
+            lambda _: jnp.inf,
+            lambda _: recur(gamma, s - 1, x),
+            operand=None
+        )
         
         # Return updated state & new gamma
         return (new_gamma, s - 1), new_gamma
@@ -45,25 +46,13 @@ def s_negative(s: ArrayLike, x: ArrayLike) -> Array:
     # Return final gamma value
     return result[0]
 
+@jit
 def custom_gammaincc(s: ArrayLike, x: ArrayLike) -> Array:
     """
-    Computes the upper incomplete gamma function Γ(s, x) for any real s and real x >= 0.
+    Computes the upper incomplete gamma function Γ(s, x) for real s in range (-3, 3),
+    where s != integer, and real x > 0.
 
-    For x = inf
-    - Returns 0.
-
-    For x = 0
-    - [s = 0]           - Return inf
-    - [s (int) < 0]     - Returns commplex inf.
-    - [s (non-int) < 0] - Returns gamma(x).
-    - [s > 0]           - Returns gamma(x).
-
-    For x > 0
-    - [s = 0] - Returns -expi(-x)
-    - [s = 0.5] - jnp.sqrt(jnp.pi) * lax.erfc(jnp.sqrt(x))
-    - [s = 1] - Uses regularised lower incomplete gamma function for positive s.
-    - [s > 0] - Uses recurrence relation for all negative s, including negative integers.
-    - [s < 0] - Handles the special case of s = 0 correctly using the exponential integral.
+    Computation is done by a recurrence relation.
 
     Parameters:
     s (float): The shape parameter.
@@ -75,4 +64,4 @@ def custom_gammaincc(s: ArrayLike, x: ArrayLike) -> Array:
     
     s, x = promote_args_inexact("custom_gammaincc", s, x)
 
-    return s_negative(s, x)
+    return compute_gamma(s, x)
